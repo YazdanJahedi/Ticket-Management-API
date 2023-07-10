0d77ed9dae5cc00a04039d055134ee510d89d1d8
Yazdan <yzdn.jt@gmail.com>
2023-07-10 15:22:39 +0330

    Change ticket template: Add a new description field

--- TicketAPI/Models/Ticket.cs
+++ TicketAPI/Models/Ticket.cs
@@ -9,6 +9,7 @@ namespace TicketAPI.Models
         public long Id { get; set; }
         public long UserId { get; set; }
         public string? Title { get; set; }
+        public string? Description { get; set; }
         public bool IsChecked { get; set; }
         public DateTime? CreateDate { get; set; }
         public DateTime? FirstResponseDate { get; set; }
