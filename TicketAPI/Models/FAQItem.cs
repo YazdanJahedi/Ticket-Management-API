b7732a5863aa4f682d25fab4db9cd946374a579b
Yazdan <yzdn.jt@gmail.com>
2023-07-10 15:18:45 +0330

    Add FAQ models

---
+++ TicketAPI/Models/FAQItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketAPI.Models
+{
+    public class FAQItem
+    {
+        public int Id { get; set; }
+        public int TitleId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ TicketAPI/Models/FAQTitle.cs
@@ -0,0 +1,8 @@
+﻿namespace TicketAPI.Models
+{
+    public class FAQTitle
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+    }
+}
