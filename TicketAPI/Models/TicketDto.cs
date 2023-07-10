ba071b29791c6cbda77ace2e226efe009aea8934
Yazdan <yzdn.jt@gmail.com>
2023-07-10 15:20:08 +0330

    Add TicketDto template

---
+++ TicketAPI/Models/TicketDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TicketAPI.Models
+{
+    public class TicketDto
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+    }
+}
